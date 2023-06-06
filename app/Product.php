a5ee0a7a36c82d32ac8654d6a36eb74da3dffebb
jollyford <118044761+jollyford@users.noreply.github.com>
2023-06-06 12:34:53 +0800

    Update Product.php

--- app/Product.php
+++ app/Product.php
@@ -1,1 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table = 'products';
+
+    protected $fillable = ['store_id', 'name', 'price', 'description'];
+}
 
